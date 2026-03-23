3bd8d67967be27dd87a0c07225f57f5d7ce2ead2
Franking31 <frankpange157@gmail.com>
2026-03-23 14:37:38 +0100

    correction d un bug

--- evaluation-day2/src/index.js
+++ evaluation-day2/src/index.js
@@ -1,30 +1,26 @@
 require("dotenv").config();
 const express = require("express");
-const authRoutes = require("./routes/auth.routes")
-const livresRoutes = require("./routes/livres.routes")
-const errorHandler = require("./middlewares/errorHandler")
-
+const authRoutes = require("./routes/auth.routes");
+const livresRoutes = require("./routes/livres.routes");
+const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-//routes
-
+// Routes
 app.use("/api/auth", authRoutes);
-app.use("/api/livres", livresRoutes)
+app.use("/api/livres", livresRoutes);
 
-//404
+// 404
+app.use((req, res) => {
+  res.status(404).json({ success: false, error: "Route non trouvee" });
+});
 
-app.use((req, res)=> {
-    res.status(404).json({ success: false, error: "Route non trouvee"});
-}); 
+// Gestionnaire d'erreurs global
+app.use(errorHandler);
 
-// Gestionnaire d erreur 
-
-app.use(errorHandler); 
-
-app.listen(PORT, ()=>{
-    console.log(`Serveur demarre sur http://localhost: ${PORT} `); 
-})
+app.listen(PORT, () => {
+  console.log(`Serveur demarre sur http://localhost:${PORT}`);
+});
