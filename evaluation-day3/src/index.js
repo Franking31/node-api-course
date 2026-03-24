78252da86576365d8af824f95434d8820fdbd6c8
franking31 <frankpange157@icloud.com>
2026-03-24 13:19:08 +0100

    restart de l eval 3

--- evaluation-day3/src/index.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const app = require("./app");
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Serveur démarré sur http://localhost:${PORT}`);
-  console.log(`Documentation Swagger : http://localhost:${PORT}/api-docs`);
-});
